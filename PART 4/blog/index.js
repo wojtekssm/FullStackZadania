7c471f43fa557f1aaca0beff7ea00e884dfac751
wojtekssm <wojtekssm@gmail.com>
2026-03-27 14:55:38 +0100

    rozpoczęcie PART 4

---
+++ PART 4/blog/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err))
+
+app.use(express.json())
+
+app.get('/', function (req, res) {
+  res.send('hello, world!')
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
