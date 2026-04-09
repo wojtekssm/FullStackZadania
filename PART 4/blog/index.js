8c91519989f89cc94149178ec0dbd874afec96db
wojtekssm <wojtekssm@gmail.com>
2026-04-09 15:00:01 +0200

    4.1 i 4.2 skończone

--- PART 4/blog/index.js
+++ PART 4/blog/index.js
@@ -1,42 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => logger.info("Connected to MongoDB"))
-  .catch(err => logger.error(err))
-
-app.use(express.json())
-
-app.get('/', function (req, res) {
-  res.send('hello, world!')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
