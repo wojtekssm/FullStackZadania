1cf19c5ae0824078a543a063f0a7a9f472f58a8b
wojtekssm <wojtekssm@gmail.com>
2026-04-16 15:16:56 +0200

    Do dokończenia

---
+++ PART 4/blog/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) =>
+    blog.likes > max.likes ? blog : max
+  )
+}
+
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog
+}
